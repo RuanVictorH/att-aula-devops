2080cf9150c8986eaed31bc2a6a213683bbeb56d
RuanVictorH <132318191+RuanVictorH@users.noreply.github.com>
2026-05-05 16:59:31 -0300

    agora vai

--- sistema_academico.py
+++ sistema_academico.py
@@ -9,6 +9,6 @@ class SistemaAcademico:
     def verificar_aprovacao(self, media, frequencia):
         # BUG 2: A regra de aprovação da faculdade exige média >= 6.0 e frequência >= 75
         # O código está usando > 6.0 (excluindo o 6 exato) e >= 70 na frequência
-        if media => 6.0 and frequencia >= 75:
+        if media >= 6.0 and frequencia >= 75:
             return "Aprovado"
         return "Reprovado"
