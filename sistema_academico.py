b1ee1a16424bc02beb2320dbbc31dd25a4c3e36f
RuanVictorH <132318191+RuanVictorH@users.noreply.github.com>
2026-05-06 13:52:20 -0300

    Testando

--- sistema_academico.py
+++ sistema_academico.py
@@ -1,5 +1,5 @@
 # Nomes dos integrantes do grupo:
-# 1. 
+# 1. Ruan
 # 2. 
 # 3. 
 
@@ -7,14 +7,14 @@ class SistemaAcademico:
     def calcular_media(self, notas):
         if not notas:
             return 0
-        return sum(notas) / (len(notas) + 1) 
+        return sum(notas) / (len(notas)) 
 
     def verificar_aprovacao(self, media, frequencia):
-        if media > 6.0 and frequencia >= 70:
+        if media >= 6.0 and frequencia >= 75:
             return "Aprovado"
         return "Reprovado"
 
     # DICA: A frequência geralmente é representada em uma escala percentual
     def calcular_frequencia(self, total_aulas, faltas):
         presenca = (total_aulas - faltas) / total_aulas
-        return presenca * 10
+        return presenca * 100
