ac2ed6f62f7aaefd41dabb399a77a39b0f7d221c
Gustavo Henrique Moraes Filho <137254291+GustavoHMFilho@users.noreply.github.com>
2026-05-07 20:27:42 -0300

    gustavo 111

--- sistema_academico.py
+++ sistema_academico.py
@@ -30,14 +30,14 @@ class SistemaAcademico:
     def calcular_media(self, notas):
         if not notas:
             return 0
-        return sum(notas) / (len(notas) + 1) 
+        return sum(notas) / (len(notas)) 
 
     def verificar_aprovacao(self, media, frequencia):
-        if media > 6.0 and frequencia >= 70:
+        if media >= 6.0 and frequencia >= 75:
             return "Aprovado"
         return "Reprovado"
 
     # DICA: A frequência geralmente é representada em uma escala percentual
     def calcular_frequencia(self, total_aulas, faltas):
         presenca = (total_aulas - faltas) / total_aulas
-        return presenca * 10
+        return presenca * 100
